43eced889b3ea99e20601739b7a25b419b30772d
swapnil-kapate <swapnilkapate2@gmail.com>
2026-07-01 19:45:50 +0530

    Initial Wedding Website

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,79 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('wedding-invitation');
+export class App implements OnInit {
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  opening = false;
+  invitationOpened = false;
+
+  weddingDate = new Date('2026-07-19T11:29:00').getTime();
+
+  days = 0;
+  hours = 0;
+  minutes = 0;
+  seconds = 0;
+
+  ngOnInit(): void {
+
+    this.updateCountdown();
+
+    setInterval(() => {
+
+      this.updateCountdown();
+      this.cdr.detectChanges();
+
+    }, 1000);
+
+  }
+
+  openInvitation() {
+
+  if (this.opening) return;
+
+  this.opening = true;
+
+  setTimeout(() => {
+
+    this.invitationOpened = true;
+    this.cdr.detectChanges();
+
+  }, 1000);
+
+}
+
+  updateCountdown(): void {
+
+    const now = new Date().getTime();
+
+    const diff = this.weddingDate - now;
+
+    if (diff < 0) {
+
+    this.days = 0;
+    this.hours = 0;
+    this.minutes = 0;
+    this.seconds = 0;
+
+    return;
+  }
+
+
+    this.days = Math.floor(diff / (1000 * 60 * 60 * 24));
+
+    this.hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+    this.minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+
+    this.seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+  }
+
 }
